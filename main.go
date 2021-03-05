adbd265e8c9420f805da7be44323e2ef13a5f2e2
Leonardo Cserny <leo.cserny@gmail.com>
2021-03-05 19:43:01 +0200

    closed log properly

--- main.go
+++ main.go
@@ -8,14 +8,17 @@ import (
 )
 
 func main() {
-	initLogging()
+	f := initLogging()
+	defer f.Close()
+
 	systray.Run(func() { notify.OnReady() }, func() { notify.OnExit() })
 }
 
-func initLogging() {
+func initLogging() *os.File {
 	file, err := os.OpenFile("drinkwater.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.SetOutput(file)
+	return file
 }
