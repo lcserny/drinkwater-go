05f5c2dd026f504b6e8178d19d8397f4ff4a3420
Leonardo Cserny <leo.cserny@gmail.com>
2021-03-17 10:56:17 +0200

    WIP: walk notify GUI

--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.16
 require (
 	github.com/getlantern/systray v1.1.0
 	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4
+	github.com/lxn/walk v0.0.0-20210112085537-c389da54e794
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
 	github.com/sirupsen/logrus v1.8.0
+	gopkg.in/Knetic/govaluate.v3 v3.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-	"drinkwater-go/notify"
-	"github.com/getlantern/systray"
+	"github.com/lxn/walk"
 	log "github.com/sirupsen/logrus"
 	"os"
 )
 
 const logFile = "drinkwater.log"
 
-func main() {
-	f := initLogging()
-	defer f.Close()
-
-	systray.Run(notify.OnReady, notify.OnExit)
-}
+//func main() {
+//	f := initLogging()
+//	defer f.Close()
+//
+//	systray.Run(notify.OnReady, notify.OnExit)
+//}
 
 func initLogging() *os.File {
 	file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
@@ -23,4 +22,74 @@ func initLogging() *os.File {
 	}
 	log.SetOutput(file)
 	return file
+}
+
+func main() {
+	f := initLogging()
+	defer f.Close()
+
+	// We need either a walk.MainWindow or a walk.Dialog for their message loop.
+	// We will not make it visible in this example, though.
+	mw, err := walk.NewMainWindow()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// We load our icon from a file.
+	icon, err := walk.Resources.Icon("stop.ico")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create the notify icon and make sure we clean it up on exit.
+	ni, err := walk.NewNotifyIcon(mw)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ni.Dispose()
+
+	// Set the icon and a tool tip text.
+	if err := ni.SetIcon(icon); err != nil {
+		log.Fatal(err)
+	}
+	if err := ni.SetToolTip("Click for info or use the context menu to exit."); err != nil {
+		log.Fatal(err)
+	}
+
+	// When the left mouse button is pressed, bring up our balloon.
+	ni.MouseDown().Attach(func(x, y int, button walk.MouseButton) {
+		if button != walk.LeftButton {
+			return
+		}
+
+		if err := ni.ShowInfo(
+			"Walk NotifyIcon Example",
+			"There are multiple ShowX methods sporting different icons."); err != nil {
+
+			log.Fatal(err)
+		}
+	})
+
+	// We put an exit action into the context menu.
+	exitAction := walk.NewAction()
+	if err := exitAction.SetText("E&xit"); err != nil {
+		log.Fatal(err)
+	}
+	exitAction.Triggered().Attach(func() { walk.App().Exit(0) })
+	if err := ni.ContextMenu().Actions().Add(exitAction); err != nil {
+		log.Fatal(err)
+	}
+
+	// The notify icon is hidden initially, so we have to make it visible.
+	if err := ni.SetVisible(true); err != nil {
+		log.Fatal(err)
+	}
+
+	// Now that the icon is visible, we can bring up an info balloon.
+	if err := ni.ShowInfo("Walk NotifyIcon Example", "Click the icon to show again."); err != nil {
+		log.Fatal(err)
+	}
+
+	// Run the message loop.
+	mw.Run()
 }
